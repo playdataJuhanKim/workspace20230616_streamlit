106e06338b5c78bfbc4e4e633e772c113302b7fb
{playdataJuhanKim} <{joohan754@gmail.com}>
2023-06-16 10:50:18 +0900

    vid img

--- app.py
+++ app.py
@@ -13,3 +13,35 @@ st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSL3WQ-4NkRIKo7li
 st.image('image/img.png')
 st.image('image/images.jpeg')
 #streamlit run app.py
+
+
+#마크다운
+
+#https://heropy.blog/2017/09/30/markdown/
+
+#제목 마크다운
+st.write('''
+# 가장 큰 제목 텍스트 (st.title)
+## 그 다음으로 큰 제목(st.header)
+### 그 다음 다음으로 큰 제목(st.subheader)
+#### 그 다음 다음 다음으로 큰 제목
+##### 그 다음 다음 다음 다음으로 큰 제목
+######## 이건없음''')
+
+#서식
+text="""
+기울임은 *별표* 또는 _언더바_
+
+진하게(볼드체) 는 **두개** __두 개__
+
+기울임+진하게 는 ***세 개*** ___세개___
+
+취소선은 ~물결표~
+
+밑줄은 <u>미잍주울</u>
+
+형광펜은 <mark>마크마크</mark>
+"""
+
+st.write(text)
+st.markdown(text, unsafe_allow_html=True)
