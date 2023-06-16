9a1706a37c782cdac2211a52b6aefefe601aa3b9
{playdataJuhanKim} <{joohan754@gmail.com}>
2023-06-16 10:18:44 +0900

    vid img

--- app.py
+++ app.py
@@ -5,5 +5,7 @@ st.title('나만의 작은 파이썬 웹페이지')
 st.header('8일차')
 st.subheader('짜파게티')
 st.write('먹고싶다.')
-
+#기능이 실행되는 순서대로 화면에서 표현. 코드상에서 윗줄부터 페이지에 반영
+st.video("https://www.youtube.com/watch?v=SaCheA6Njc4")
+st.image("https://i.imgur.com/jorp5JH.png")
 #streamlit run app.py
