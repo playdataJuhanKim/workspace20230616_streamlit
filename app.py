3228ef0b91112e2e8dc5b437f446b11b9e445d40
{playdataJuhanKim} <{joohan754@gmail.com}>
2023-06-16 11:28:32 +0900

    vid img

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ st.write('''
 #### 그 다음 다음 다음으로 큰 제목
 ##### 그 다음 다음 다음 다음으로 큰 제목
 ######## 이건없음''')
-
+st.divider()
 #서식
 text="""
 기울임은 *별표* 또는 _언더바_
@@ -45,3 +45,74 @@ text="""
 
 st.write(text)
 st.markdown(text, unsafe_allow_html=True)
+
+st.divider()
+
+#레이아웃
+st.subheader("레이아웃")
+st.write('''
+    ### 순서가 없는 리스트
+    *별표를 여백 1칸 이상과 사용하면 순서가 없는 리스트.
+    * 별표를 여백 1칸 이상과 사용하면 순서가 없는 리스트.
+        * 들여쓰기 1
+            * 들여들여쓰기
+    (-)도 됨
+        - 쓰기쓰기
+    ### 순서가 있는 리스트
+    1. 순서가
+    2. 있는
+    3. 리스트 
+        4.들여
+            9.쓰기 (숫자를 건너뛰어도 순서를 따름)
+    1. 들여쓰기는
+        1. 1로시작해야됨
+    1. 1부터 증가함
+
+    ---
+    가로줄로 나눔
+    ---
+    ###테이블(표) 만들기
+    |이름|직업|
+    |---|---|
+    |파이썬|백수|
+''')
+
+#링크
+# st.divider()
+# st.subheader('링크')
+# st.write('''
+#     * [표시할 텍스트](주소)
+#     * [연습문제](https://solved.ac/)
+#     * ![이미지에 대한 설명](jetbrains://pycharm/navigate/reference?project=workspace20230616_streamlit&path=image/images.jpeg)
+#     * 이미지에 링크를 넣으려면
+#     * [image/img.png](https://github.com/playdataJuhanKim/workspace20230616_streamlit/blob/106e06338b5c78bfbc4e4e633e772c113302b7fb/image/img.png)
+# ''')
+#
+# st.write(f"""
+#     * [표시할 텍스트](https://naver.com)
+#     * [표시할 텍스트]({l1})
+#     * ![이미지에 대한 설명](https://cdn.pixabay.com/photo/2023/04/08/18/01/flower-7909902_640.jpg)
+#     * ![이미지에 대한 설명]({l2})
+#     * [![이미지에 대한 설명](https://cdn.pixabay.com/photo/2023/04/08/18/01/flower-7909902_640.jpg)](https://naver.com))
+# """)
+
+#인용
+st.divider()
+st.subheader('인용')
+st.write('''
+    > 무언가 멋진 말 - 유명한 사람
+    
+    > 대충 흑백사진에 글쓰면 명언이다 - 침착맨
+    
+    > 인용 첫줄
+        > > 인용문 안에 인용
+    #### 코드
+    `(1옆에 이거)`
+    ```
+    이걸로 여러줄 코드를 나타내고
+    줄바꿈도?
+    print("짜장면")
+    ```
+    ```python
+    print("파이썬!")
+    ''')
